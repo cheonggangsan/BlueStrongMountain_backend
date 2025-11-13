84963c74a89d04b7ed248c5cec71acb586a1ed8b
junyonglee0223 <ljyong3339@gmail.com>
2025-11-13 17:53:53 +0900

    feat: project init

---
+++ src/main/java/com/bsm/BlueStrongMountain/BlueStrongMountainApplication.java
@@ -0,0 +1,13 @@
+package com.bsm.BlueStrongMountain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueStrongMountainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueStrongMountainApplication.class, args);
+	}
+
+}
