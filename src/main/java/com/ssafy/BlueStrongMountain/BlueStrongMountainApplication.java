86f4b9740231aa2ca2420496671e475fb7537f3a
junyonglee0223 <ljyong3339@gmail.com>
2025-11-21 16:39:29 +0900

    init: init spring project

---
+++ src/main/java/com/ssafy/BlueStrongMountain/BlueStrongMountainApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.BlueStrongMountain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueStrongMountainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueStrongMountainApplication.class, args);
+	}
+
+}
