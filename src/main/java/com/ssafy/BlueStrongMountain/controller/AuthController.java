3ddbbb968dd17181438ff96452a7726b4ff6ab60
junyonglee0223 <ljyong3339@gmail.com>
2025-12-08 11:44:38 +0900

    [BST-13] feat: auth controller, user controller 구현

---
+++ src/main/java/com/ssafy/BlueStrongMountain/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.ssafy.BlueStrongMountain.controller;
+
+import com.ssafy.BlueStrongMountain.dto.*;
+import com.ssafy.BlueStrongMountain.service.AuthService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@AllArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest req) {
+        return ResponseEntity.ok(authService.login(req));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest req){
+
+        //test
+        System.out.println("!!!!!!!!!register user");
+        System.out.println(req.toString());
+
+        return ResponseEntity.ok(authService.register(req));
+    }
+
+    @GetMapping("/duplicate/username")
+    public ResponseEntity<UsernameDuplicateResponse> dup(
+            @RequestParam String username
+    ) {
+        return ResponseEntity.ok(authService.checkUsername(username));
+    }
+}
