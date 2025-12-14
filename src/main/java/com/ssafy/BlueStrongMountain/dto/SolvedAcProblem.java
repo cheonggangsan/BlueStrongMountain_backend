1923c42284a5105f15c417c64dada265286c3ca3
junyonglee0223 <ljyong3339@gmail.com>
2025-12-14 21:09:15 +0900

    [BST-32] feature: response dto 정의

---
+++ src/main/java/com/ssafy/BlueStrongMountain/dto/SolvedAcProblem.java
@@ -0,0 +1,10 @@
+package com.ssafy.BlueStrongMountain.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SolvedAcProblem {
+    private Long problemId;
+}
