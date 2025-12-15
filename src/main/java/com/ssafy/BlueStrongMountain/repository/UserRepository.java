b888892bb2f3b80cd0527e71734a2e35b0bd0365
junyonglee0223 <94211484+junyonglee0223@users.noreply.github.com>
2025-12-15 17:22:58 +0900

    [BST-40] feat: 유저 검색 API 추가 (username/email 부분 검색) (#41)
    * [BST-40] feat: keyword로 username, email 포함 객체 탐색 로직 구현
    
    * [BST-40] feat: keyword로 username, email 포함 객체 탐색 로직 구현
    repository 추가
    
    * [BST-40] feat: 반환 dto 구현
    
    * [BST-40] feat: keyword로 email, username 탐색 service 구현
    
    * [BST-40] feat: keyword로 email, username 탐색 controller 구현
    
    * [BST-40] docs: swagger 추가
    
    * [BST-40] fix: userMapper where 오타 수정

--- src/main/java/com/ssafy/BlueStrongMountain/repository/UserRepository.java
+++ src/main/java/com/ssafy/BlueStrongMountain/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.ssafy.BlueStrongMountain.repository;
 
 import com.ssafy.BlueStrongMountain.domain.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository {
@@ -9,5 +10,7 @@ public interface UserRepository {
     Optional<User> findByEmail(String email);
     Optional<User> findById(Long id);
     Optional<User> findByUsername(String username);
+    List<User> searchByEmail(String keyword);
+    List<User> searchByUsername(String keyword);
     void delete(Long id);
 }
