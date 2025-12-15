9cb6dca9d2796e48b4fb336f4ca26018e32f4b19
junyonglee0223 <ljyong3339@gmail.com>
2025-12-15 13:23:15 +0900

    [BST-18] fix: UserSolutionRepository 해당 userSolution이 존재하면 find해서 반환

--- src/main/java/com/ssafy/BlueStrongMountain/repository/UserSolutionRepositoryImpl.java
+++ src/main/java/com/ssafy/BlueStrongMountain/repository/UserSolutionRepositoryImpl.java
@@ -16,9 +16,10 @@ public class UserSolutionRepositoryImpl implements UserSolutionRepository {
 
     @Override
     public UserSolution save(UserSolution userSolution) {
-        if(findByUserAndProblem(userSolution.getUserId(), userSolution.getProblemId())
-                .isPresent()){
-            return userSolution;
+        Optional<UserSolution> existingUserSolution
+                = findByUserAndProblem(userSolution.getUserId(), userSolution.getProblemId());
+        if(existingUserSolution.isPresent()){
+            return existingUserSolution.get();
         }
         mapper.save(userSolution);
         return userSolution;
