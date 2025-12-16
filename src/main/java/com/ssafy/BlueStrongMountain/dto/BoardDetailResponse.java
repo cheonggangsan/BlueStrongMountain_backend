f07730530cccd21fd91b375838879cbe6feeb9aa
junyonglee0223 <ljyong3339@gmail.com>
2025-12-16 12:04:48 +0900

    [BST-45] feat: boardDetail service, controller 반환 객체 수정

--- src/main/java/com/ssafy/BlueStrongMountain/dto/BoardDetailResponse.java
+++ src/main/java/com/ssafy/BlueStrongMountain/dto/BoardDetailResponse.java
@@ -1,5 +1,6 @@
 package com.ssafy.BlueStrongMountain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -8,20 +9,23 @@ import java.util.List;
 
 @Getter
 @ToString
+@AllArgsConstructor
 public class BoardDetailResponse {
 
     private Long boardId;
     private String title;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
-    private List<Long> problemIds;
+    //private List<Long> problemIds;
+    private List<ProblemSimpleDto> problems;
 
-    public BoardDetailResponse(Long boardId, String title, LocalDateTime startTime,
-                               LocalDateTime endTime, List<Long> problemIds) {
-        this.boardId = boardId;
-        this.title = title;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.problemIds = problemIds;
-    }
+
+//    public BoardDetailResponse(Long boardId, String title, LocalDateTime startTime,
+//                               LocalDateTime endTime, List<Long> problemIds) {
+//        this.boardId = boardId;
+//        this.title = title;
+//        this.startTime = startTime;
+//        this.endTime = endTime;
+//        this.problemIds = problemIds;
+//    }
 }
