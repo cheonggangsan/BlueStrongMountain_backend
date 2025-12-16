694fbba1365f41ae2d733f8f7f3c9c09e326a840
junyonglee0223 <ljyong3339@gmail.com>
2025-12-16 20:12:41 +0900

    [BST-39] refactoring: GroupUpdateRequest managerIds, membersIds를 결과로 입력받는 request

--- src/main/java/com/ssafy/BlueStrongMountain/dto/GroupUpdateRequest.java
+++ src/main/java/com/ssafy/BlueStrongMountain/dto/GroupUpdateRequest.java
@@ -4,12 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class GroupUpdateRequest {
 
     private String title;
+    private List<Long> managerIds;
+    private List<Long> memberIds;
     private String visibility;
     private String description;
 }
