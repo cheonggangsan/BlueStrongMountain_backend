730a66d74625629403406e70333d7e73894aa65c
junyonglee0223 <ljyong3339@gmail.com>
2025-12-16 14:01:32 +0900

    [BST-45] refactoring: 주석제거

--- src/main/java/com/ssafy/BlueStrongMountain/dto/BoardDetailResponse.java
+++ src/main/java/com/ssafy/BlueStrongMountain/dto/BoardDetailResponse.java
@@ -16,16 +16,5 @@ public class BoardDetailResponse {
     private String title;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
-    //private List<Long> problemIds;
     private List<ProblemSimpleDto> problems;
-
-
-//    public BoardDetailResponse(Long boardId, String title, LocalDateTime startTime,
-//                               LocalDateTime endTime, List<Long> problemIds) {
-//        this.boardId = boardId;
-//        this.title = title;
-//        this.startTime = startTime;
-//        this.endTime = endTime;
-//        this.problemIds = problemIds;
-//    }
 }
