c3388caa868ffd0410bdda3c55679ee8546e078d
junyonglee0223 <94211484+junyonglee0223@users.noreply.github.com>
2025-12-16 14:16:54 +0900

    [BST-45] refactoring: board detail 응답에 문제 요약 DTO 추가 (#46)
    * [BST-45] feat: problemSimpleDto 구현
    id(문제번호), title, difficulty 저장
    
    * [BST-45] feat: boardDetail service, controller 반환 객체 수정
    
    * [BST-45] feat: boardDetail service 수정
    problemRepository 추가해서 problem 정보로 Dto 새로 생성
    
    * [BST-45] docs: swagger yaml 수정
    
    * [BST-45] refactoring: 주석제거

--- src/main/java/com/ssafy/BlueStrongMountain/dto/BoardDetailResponse.java
+++ src/main/java/com/ssafy/BlueStrongMountain/dto/BoardDetailResponse.java
@@ -1,5 +1,6 @@
 package com.ssafy.BlueStrongMountain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -8,20 +9,12 @@ import java.util.List;
 
 @Getter
 @ToString
+@AllArgsConstructor
 public class BoardDetailResponse {
 
     private Long boardId;
     private String title;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
-    private List<Long> problemIds;
-
-    public BoardDetailResponse(Long boardId, String title, LocalDateTime startTime,
-                               LocalDateTime endTime, List<Long> problemIds) {
-        this.boardId = boardId;
-        this.title = title;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.problemIds = problemIds;
-    }
+    private List<ProblemSimpleDto> problems;
 }
