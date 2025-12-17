3e9ee2f167863b781eb7cba44bb3ce9f272be2be
junyonglee0223 <ljyong3339@gmail.com>
2025-12-17 14:39:46 +0900

    [BST-57] fix: userId 1로 하드코딩 부분 수정 전체 유저 id 가져와서 업데이트 반영

--- src/main/java/com/ssafy/BlueStrongMountain/scheduler/SolvedAcScheduler.java
+++ src/main/java/com/ssafy/BlueStrongMountain/scheduler/SolvedAcScheduler.java
@@ -1,14 +1,12 @@
 package com.ssafy.BlueStrongMountain.scheduler;
 
 import com.ssafy.BlueStrongMountain.domain.User;
-import com.ssafy.BlueStrongMountain.exception.UserNotFoundException;
 import com.ssafy.BlueStrongMountain.repository.UserRepository;
 import com.ssafy.BlueStrongMountain.service.SolvedAcSyncService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
@@ -20,16 +18,28 @@ public class SolvedAcScheduler {
     //30초에 한번씩 반복
     @Scheduled(fixedDelay = 30 * 1000)
     public void syncAllUsersSolvedProblems(){
-        User initUser = userRepository.findById(1L)
-                .orElseThrow(UserNotFoundException::new);
+
+
+        List<Long> userIds = userRepository.findAll()
+                .stream()
+                .map(User::getId)
+                .toList();
+
+        for(Long userId : userIds){
+            //FIXME handle을 전달하는게 더 바람직하지 않나?????
+            solvedAcSyncService.syncUserSolution(userId);
+        }
+
+//        User initUser = userRepository.findById(1L)
+//                .orElseThrow(UserNotFoundException::new);
 
 //        long startTime = System.nanoTime();//test
 //
 //        List<Long> findNewProblemIds =
 //                solvedAcSyncService.testFetchProblemIds(initUser.getId());
 
-
-        solvedAcSyncService.syncUserSolution(initUser.getId());
+//
+//        solvedAcSyncService.syncUserSolution(initUser.getId());
 
 //        long endTime = System.nanoTime();//test
 //        long elapsedMs = (endTime - startTime) / 1_000_000;
