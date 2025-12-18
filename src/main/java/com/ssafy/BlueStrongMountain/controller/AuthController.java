d8baee5bc3df027117fd7a4ad1b7f81909d9db98
junyonglee0223 <94211484+junyonglee0223@users.noreply.github.com>
2025-12-18 17:31:45 +0900

    [BST-68] refactoring: baekjoon handle 확인 authController에서 처리하도록 변경 (#69)
    * [BST-68] refactoring: baekjoon handle 확인 authController에서 처리하도록 변경
    
    * [BST-68] docs: swagger yaml 수정

--- src/main/java/com/ssafy/BlueStrongMountain/controller/AuthController.java
+++ src/main/java/com/ssafy/BlueStrongMountain/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.ssafy.BlueStrongMountain.controller;
 
 import com.ssafy.BlueStrongMountain.dto.*;
 import com.ssafy.BlueStrongMountain.service.AuthService;
+import com.ssafy.BlueStrongMountain.service.SolvedAcSyncService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final SolvedAcSyncService solvedAcSyncService;
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest req) {
@@ -34,6 +36,17 @@ public class AuthController {
             @RequestParam String username
     ) {
         return ResponseEntity.ok(authService.checkUsername(username));
+    }
+    /**
+     * solved.ac baekjoon handle 확인 테스트
+     *
+     *
+     */
+    @GetMapping("/existHandle")
+    public ResponseEntity<Boolean> checkBaekHandle(
+            @RequestParam String handle
+    ){
+        return ResponseEntity.ok(solvedAcSyncService.existSolvedAcUser(handle));
     }
 
     @PostMapping("/logout")
