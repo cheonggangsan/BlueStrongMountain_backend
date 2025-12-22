014e8383a2cc294f8dd77b454480288f76ca2de5
junyonglee0223 <ljyong3339@gmail.com>
2025-12-22 22:27:51 +0900

    [BST-82] feat: 필터링 기능 db에서 적용 findProblems -> 기본 필터링 기능 적용 problems, problem_tags, tags table join으로 활용 tag, minSolvers, difficulty
    findUnsolvedProblemsByGroup
    -> 유저 미해결 문제 필터링 적용
    user_group, user_solution join해서 해당 문제들 제외 조건 적용

--- src/main/java/com/ssafy/BlueStrongMountain/repository/ProblemRepository.java
+++ src/main/java/com/ssafy/BlueStrongMountain/repository/ProblemRepository.java
@@ -1,6 +1,8 @@
 package com.ssafy.BlueStrongMountain.repository;
 
 import com.ssafy.BlueStrongMountain.dto.ProblemDto;
+import com.ssafy.BlueStrongMountain.dto.ProblemFilterCondition;
+
 import java.util.List;
 import java.util.Set;
 
@@ -9,4 +11,9 @@ public interface ProblemRepository {
 
     List<ProblemDto> findByIdList(List<Long> ids);
 
+    List<ProblemDto> findByCondition(
+            Long groupId,
+            boolean unsolved,
+            ProblemFilterCondition cond
+    );
 }
