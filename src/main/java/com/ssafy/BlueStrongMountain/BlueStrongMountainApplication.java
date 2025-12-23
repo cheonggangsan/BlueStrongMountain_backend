d962878a6c2f7e2e6f679aa82def1a8487dfdd74
DonghanPark <vkfkdtor00@gmail.com>
2025-12-23 13:55:24 +0900

    [BSM-79] chore: exclude OpenAI auto-configuration to avoid conflicts

--- src/main/java/com/ssafy/BlueStrongMountain/BlueStrongMountainApplication.java
+++ src/main/java/com/ssafy/BlueStrongMountain/BlueStrongMountainApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.ai.autoconfigure.openai.OpenAiAutoConfiguration.class
+})
 @EnableScheduling
 public class BlueStrongMountainApplication {
 
