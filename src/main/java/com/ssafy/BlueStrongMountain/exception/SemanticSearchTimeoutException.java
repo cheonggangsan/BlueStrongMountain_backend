06e085e71a9622d0add9a418370022e4b0139cc0
junyonglee0223 <94211484+junyonglee0223@users.noreply.github.com>
2025-12-24 19:22:39 +0900

    [BST-92] feat: option 2 ai 적용 semantic search 재시도 및 예외 처리 강화 (#93)
    * [BST-92] feat: embedding ai 추천 로직 option 2로 추가
    
    * [BST-92] feat: ai embedding exception 추가

---
+++ src/main/java/com/ssafy/BlueStrongMountain/exception/SemanticSearchTimeoutException.java
@@ -0,0 +1,6 @@
+package com.ssafy.BlueStrongMountain.exception;
+
+public class SemanticSearchTimeoutException extends RuntimeException {
+    public SemanticSearchTimeoutException(String s) {
+    }
+}
